214ed9481f06772aaacdb66fa02760cd7464e51c
doko <dokotheo@gmail.com>
2020-05-17 19:03:50 +0700

    Added Login by Sql User
    Signed-off-by: doko <dokotheo@gmail.com>

--- iam-reportEjb/ejbModule/ca/iam/eao/LoginEao.java
+++ iam-reportEjb/ejbModule/ca/iam/eao/LoginEao.java
@@ -1,5 +1,8 @@
 package ca.iam.eao;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -18,12 +21,13 @@ public class LoginEao {
 	Connection conn = null;
 	String query = null;
 
-	public String getUser(String user,String pass) throws SQLException{
+	public String getUser(String user,String pass) throws SQLException, NoSuchAlgorithmException{
+		pass = genPass(pass);
 		try{
 			result = "0";
 			
 			conn = Settings.getConnection();
-			query = "{call getUserDbmc(?,?)}";
+			query = "{call getUserLogin(?,?)}";
 			cs = conn.prepareCall(query);
 			cs.setString(1,user);
 			cs.setString(2,pass);
@@ -45,17 +49,32 @@ public class LoginEao {
 		return result;
 	}
 
-	public void setLogin(String user,String pass) throws SQLException{
-		try{		
-			conn = Settings.getConnection();
-			query = "{call setUserDbmc(?,?)}";
-			cs = conn.prepareCall(query);
-			cs.setString(1,user);
-			cs.setString(2,pass);
-			cs.execute();
-		}
-		finally{
-			conn.close();
-		}
+//	public void setLogin(String user,String pass) throws SQLException{
+//		try{		
+//			conn = Settings.getConnection();
+//			query = "{call setUserLogout(?,?)}";
+//			cs = conn.prepareCall(query);
+//			cs.setString(1,user);
+//			cs.setString(2,pass);
+//			cs.execute();
+//		}
+//		finally{
+//			conn.close();
+//		}
+//	}
+
+	public String genPass(String pass) throws NoSuchAlgorithmException{
+		String result = "";
+		String password = pass;
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hashInBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashInBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        System.out.println("MD5: " + sb.toString());
+		return result;
 	}
 }
